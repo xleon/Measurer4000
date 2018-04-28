07b4c660c2f9b790a7850619afc62c959f41c903
Diego Ponce de León <malandro@gmail.com>
2018-04-29 01:14:10 +0200

    Rename projects and fix references to the new Core netstandard project

--- Measurer4000.Core2/Services/Interfaces/IFileManagerService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IO;
-
-namespace Measurer4000.Core.Services.Interfaces
-{
-    public interface IFileManagerService
-    {
-        Stream OpenRead(string filePath);
-    }
-}
--- Measurer4000.Core2/Services/Interfaces/IWebBrowserTaskService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Measurer4000.Core.Services.Interfaces
-{
-    public interface IWebBrowserTaskService
-    {
-        void Navigate(string url);
-    }
-}
